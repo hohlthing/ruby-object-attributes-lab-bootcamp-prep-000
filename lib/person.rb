8d3f40208841305c10ac8113b4995c9581701e3b
michael hohl <mhohl@rocketmail.com>
2018-03-09 19:51:57 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,10 +5,10 @@ class Person
   def name=(new_name)
     @name = new_name
   end
-    def breed
-    @breed
+    def job
+    @job
   end
-  def breed=(new_breed)
-    @breed = new_breed
+  def job=(new_job)
+    @job = new_job
   end
 end
